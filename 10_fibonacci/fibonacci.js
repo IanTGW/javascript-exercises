38680badfdf2e29ad4b0711e85920eadae2fe503
IanTGW <ianwuxiinau@gmail.com>
2023-09-19 05:42:27 +1000

    Complete 08 to 12 exercise in arrays

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(length) {
+    if (length < 0){
+        return "OOPS";
+    }
+    let sum = [1,1];
+    for( i = 1; i < length - 1; i++ ){
+    sum.push(sum[i] + sum[i-1])
+    }
+    return sum[length - 1]
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    array.forEach((person) => {
+        if (!person.yearOfDeath){
+            let date = new Date;
+            person.yearOfDeath = date.getFullYear();
+        }
+        console.log(person.yearOfDeath)
+    })
+    array.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge > bAge){
+            return 1;
+        } else {
+            return -1;
+        }
+        
+    })
+    console.log(array)
+    return array[array.length-1]
+    
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
